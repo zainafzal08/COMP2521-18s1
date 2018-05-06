9501db79655f8c2e451909c37d0f50ae9ea288cd
Zain Afzal <zain.afz@gmail.com>
2018-05-07 01:09:35 +1000

    Added Week 10 stuff

---
+++ week_10/M09A/bst.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct BSTNode *Link;
+typedef struct BSTNode *BSTree;
+
+typedef struct BSTNode {
+   int  value;
+   Link left, right;
+} BSTNode;
+
+Link newNode(int v) {
+  Link n = malloc(sizeof(BSTNode));
+  n->value = v;
+  n->left = NULL;
+  n->right = NULL;
+  return n;
+}
+
+
+int BSTWidth(BSTree t) {
+  return 0;
+}
+
+
+int main(int argc, char* argv[]) {
+  Link t = newNode(4);
+  t->left = newNode(2);
+  t->left->right = newNode(3);
+  t->right = newNode(5);
+  t->right->right = newNode(6);
+  assert(BSTWidth(t) == 15);
+}
---
+++ week_10/welcome.md
@@ -0,0 +1,22 @@
+# Welcome!
+---
+
+#### Introduction
+
+Whats up!
+
+1. Any questions from last week
+2. How you feeling
+
+#### Questions
+
+1. What is infix, postfix, level order
+2. why recurrsion?
+3. usefulness of traversal.
+
+::>> http://cgi.cse.unsw.edu.au/~cs2521/18s1/tutes/week10/index.php?view=qo
+
+#### Lab
+
+1. BSTreeLevelOrder
+2.
